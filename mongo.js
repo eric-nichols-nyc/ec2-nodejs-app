c1d5097fd1ebddb1394c632aab98123d5a7466df
Eric Nichols <ebn646@hotmail.com>
2024-08-23 15:46:44 -0400

    initial commit

---
+++ mongo.js
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb";
+import {} from 'dotenv/config'
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+const connectionString = `mongodb+srv://ebn646:${password}@devcluster.2sfov.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`; // clustore url
+const client = new MongoClient(connectionString);
+let conn;
+try {
+  conn = await client.connect();
+  console.log("connection successful")
+} catch(e) {
+  console.error(e);
+}
+let db = conn.db("integration_ninjas");
+export default db;
